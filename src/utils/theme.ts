f31358d2fd0392f0987ca78b03ca06ea57648158
若林勇汰 <wnyuukure@gmail.com>
2019-06-01 12:35:34 +0900

    [Task] Dashboard実装

---
+++ src/utils/theme.ts
@@ -0,0 +1,7 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import orange from '@material-ui/core/colors/orange';
+export const theme = createMuiTheme({
+  palette: {
+    primary: orange,
+  },
+});
