df9d19665b13b7e9a2adc334af6fa0282124c274
若林勇汰 <wnyuukure@gmail.com>
2019-06-08 11:27:22 +0900

    [style] 社員のテーブルを修正した

--- src/components/JuniorFeelings/JuniorFeelings.tsx
+++ src/components/JuniorFeelings/JuniorFeelings.tsx
@@ -1,227 +1,135 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import { AutoSizer, Column, Table, TableCellRenderer, TableHeaderProps } from 'react-virtualized';
+import Avatar from '@material-ui/core/Avatar';
 
-const styles = (theme: Theme) =>
+const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    flexContainer: {
+    root: {
+      width: '100%',
+      marginTop: theme.spacing(3),
+      overflowX: 'auto',
+    },
+    table: {
+      minWidth: 650,
+    },
+    row: {
       display: 'flex',
-      alignItems: 'center',
-      boxSizing: 'border-box',
+      flexDirection: 'row',
     },
-    tableRow: {
-      cursor: 'pointer',
-    },
-    tableRowHover: {
-      '&:hover': {
-        backgroundColor: theme.palette.grey[200],
-      },
-    },
-    tableCell: {
-      flex: 1,
-    },
-    noClick: {
-      cursor: 'initial',
-    },
-  });
+    headerDate: {
+      display: 'flex',
+      flexDirection: 'column', 
+    }, 
+    dateMaginTop: {
+      marginTop: -15
+    }
+  }),
+);
 
-interface ColumnData {
-  dataKey: string;
-  label: string;
-  numeric?: boolean;
-  width: number;
-}
-
-interface Row {
-  index: number;
-}
-
-interface JuniorFeelingsTableProps extends WithStyles<typeof styles> {
-  columns: ColumnData[];
-  headerHeight: number;
-  onRowClick?: () => void;
-  rowCount: number;
-  rowGetter: (row: Row) => Data;
-  rowHeight: number;
-}
-
-class JuniorFeelings extends React.PureComponent<JuniorFeelingsTableProps> {
-  static defaultProps = {
-    headerHeight: 64,
-    rowHeight: 64,
-  };
-
-  getRowClassName = ({ index }: Row) => {
-    const { classes, onRowClick } = this.props;
-
-    return clsx(classes.tableRow, classes.flexContainer, {
-      [classes.tableRowHover]: index !== -1 && onRowClick != null,
-    });
-  };
-
-  cellRenderer: TableCellRenderer = ({ cellData, columnIndex }) => {
-    const { columns, classes, rowHeight, onRowClick } = this.props;
-    return (
-      <TableCell
-        component="div"
-        className={clsx(classes.tableCell, classes.flexContainer, {
-          [classes.noClick]: onRowClick == null,
-        })}
-        variant="body"
-        style={{ height: rowHeight }}
-        align={(columnIndex != null && columns[columnIndex].numeric) || false ? 'right' : 'left'}
-      >
-        {cellData}
-      </TableCell>
-    );
-  };
-
-  headerRenderer = ({ label, columnIndex }: TableHeaderProps & { columnIndex: number }) => {
-    const { headerHeight, columns, classes } = this.props;
-
-    return (
-      <TableCell
-        component="div"
-        className={clsx(classes.tableCell, classes.flexContainer, classes.noClick)}
-        variant="head"
-        style={{ height: headerHeight }}
-        align={columns[columnIndex].numeric || false ? 'right' : 'left'}
-      >
-        <span>{label}</span>
-      </TableCell>
-    );
-  };
-
-  render() {
-    const { classes, columns, ...tableProps } = this.props;
-    return (
-      <AutoSizer>
-        {({ height, width }) => (
-          <Table height={height} width={width} {...tableProps} rowClassName={this.getRowClassName}>
-            {columns.map(({ dataKey, ...other }, index) => {
-              return (
-                <Column
-                  key={dataKey}
-                  headerRenderer={headerProps =>
-                    this.headerRenderer({
-                      ...headerProps,
-                      columnIndex: index,
-                    })
-                  }
-                  className={classes.flexContainer}
-                  cellRenderer={this.cellRenderer}
-                  dataKey={dataKey}
-                  {...other}
-                />
-              );
-            })}
-          </Table>
-        )}
-      </AutoSizer>
-    );
-  }
-}
-
-(JuniorFeelings as any).propTypes = {
-  classes: PropTypes.object.isRequired,
-  columns: PropTypes.arrayOf(PropTypes.object).isRequired,
-  headerHeight: PropTypes.number,
-  onRowClick: PropTypes.func,
-  rowHeight: PropTypes.number,
+type RowData = {
+  junior: string;
+  weekFeelings?: {
+    morning?: JSX.Element;
+    evening?: JSX.Element;
+  }[];
 };
 
-const VirtualizedTable = withStyles(styles)(JuniorFeelings);
-
-// ---
-
-interface Data {
-  id: number;
-  junior: string;
-  monday: number;
-  tuesday: number;
-  wednesday: number;
-  thursday: number;
-  friday: number;
-}
-type Sample = [string, number, number, number, number, number];
-
-const sample: Sample[] = [
-  ['髙橋 佑太', 10, 16.0, 24, 6.0, 5.0],
-  ['戸澤 涼', 159, 6.0, 24, 4.0, 5.0],
-  ['若林 勇汰', 237, 9.0, 37, 4.3, 5.0],
-  ['岡田 将太朗', 262, 16.0, 24, 6.0, 5.0],
-  ['白幡 祐叶', 305, 3.7, 67, 4.3, 5.0],
+const rows: RowData[] = [
+  {
+    junior: '髙橋 佑太',
+    weekFeelings: [
+      {
+        morning: <Avatar src='https://mentalcaredevstorage.blob.core.windows.net/cocoa-web-resource/round_sentiment_dissatisfied_black_18dp.png' />,
+        evening: <Avatar src='https://mentalcaredevstorage.blob.core.windows.net/cocoa-web-resource/round_sentiment_dissatisfied_black_18dp.png' />
+      },
+      {
+        morning: <Avatar src='' />,
+        evening: <Avatar src='' />
+      },
+      {
+        morning: <Avatar src='' />,
+        evening: <Avatar src='' />
+      },
+      {
+        morning: <Avatar src='' />,
+        evening: <Avatar src='' />
+      },
+      {
+        morning: <Avatar src='' />,
+        evening: <Avatar src='' />
+      }
+    ],
+  }
 ];
 
-function createData(
-  id: number,
-  junior: string,
-  monday: number,
-  tuesday: number,
-  wednesday: number,
-  thursday: number,
-  friday: number,
-): Data {
-  return { id, junior, monday, tuesday, wednesday, thursday, friday };
-}
+function SimpleTable() {
+  const classes = useStyles();
 
-const rows: Data[] = [];
-
-for (let i = 0; i < 200; i += 1) {
-  const randomSelection = sample[Math.floor(Math.random() * sample.length)];
-  rows.push(createData(i, ...randomSelection));
-}
-
-function ReactVirtualizedTable() {
   return (
-    <Paper style={{ height: 650, width: '100%' }}>
-      <VirtualizedTable
-        rowCount={rows.length}
-        rowGetter={({ index }) => rows[index]}
-        columns={[
-          {
-            width: 300,
-            label: 'Junior',
-            dataKey: 'junior',
-          },
-          {
-            width: 180,
-            label: '6/01',
-            dataKey: 'monday',
-            numeric: true,
-          },
-          {
-            width: 180,
-            label: '6/02',
-            dataKey: 'tuesday',
-            numeric: true,
-          },
-          {
-            width: 180,
-            label: '6/03',
-            dataKey: 'wednesday',
-            numeric: true,
-          },
-          {
-            width: 180,
-            label: '6/04',
-            dataKey: 'thursday',
-            numeric: true,
-          },
-          {
-            width: 180,
-            label: '6/05',
-            dataKey: 'friday',
-            numeric: true,
-          },
-        ]}
-      />
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell align='center'><h2>社員</h2></TableCell>
+            <TableCell align="center">
+              <div className={classes.headerDate}>
+                <h2>6/3</h2>
+                <h2 className={classes.dateMaginTop}>朝 夕</h2>
+              </div>
+              </TableCell>
+            <TableCell align="center">
+            <div className={classes.headerDate}>
+                <h2>6/4</h2>
+                <h2 className={classes.dateMaginTop}>朝 夕</h2>
+              </div>
+            </TableCell>
+            <TableCell align="center">
+            <div className={classes.headerDate}>
+                <h2>6/5</h2>
+                <h2 className={classes.dateMaginTop}>朝 夕</h2>
+              </div>
+            </TableCell>
+            <TableCell align="center">
+            <div className={classes.headerDate}>
+                <h2>6/6</h2>
+                <h2 className={classes.dateMaginTop}>朝 夕</h2>
+              </div>
+            </TableCell>
+            <TableCell align="center">
+            <div className={classes.headerDate}>
+                <h2>6/7</h2>
+                <h2 className={classes.dateMaginTop}>朝 夕</h2>
+              </div>
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow key={row.junior}>
+              <TableCell component="th" scope="row" align='center'>
+                {row.junior}
+              </TableCell>
+              {row.weekFeelings && row.weekFeelings.map(icons => (
+                <TableCell align='center'>
+                  <div className={classes.row}>
+                  {icons.morning && icons.morning}
+                  {icons.evening && icons.evening}
+                  </div>
+                </TableCell>
+                ))
+              }
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </Paper>
   );
 }
 
-export default ReactVirtualizedTable;
+export default SimpleTable;
