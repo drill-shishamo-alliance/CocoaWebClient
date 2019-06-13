fb49d6840cdf3620aa10c1a57e5bd9fd298e58e2
髙橋 佑太 <yuta.takahashi@jbs.com>
2019-06-13 16:56:46 +0900

    [chore] sagaの起動設定

--- src/index.tsx
+++ src/index.tsx
@@ -9,14 +9,18 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
 import { createLogger } from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from 'src/sagas';
 
 const composeEnhancers = composeWithDevTools({
   // Specify name here, actionsBlacklist, actionsCreators and other options if needed
 });
 
 const logger = createLogger();
-const middleware = [logger];
+const sagaMiddleware = createSagaMiddleware();
+const middleware = [logger, sagaMiddleware];
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middleware)));
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
