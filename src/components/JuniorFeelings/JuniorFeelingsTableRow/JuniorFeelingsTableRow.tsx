ddfb2b5915c15b5068e1403d7f7c1db08aaccfaf
若林勇汰 <wnyuukure@gmail.com>
2019-07-10 14:05:02 +0900

    [fix] 名前変更

--- src/components/JuniorFeelings/JuniorFeelingsTableRow/JuniorFeelingsTableRow.tsx
+++ src/components/JuniorFeelings/JuniorFeelingsTableRow/JuniorFeelingsTableRow.tsx
@@ -5,7 +5,7 @@ import { withStyles } from '@material-ui/styles';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import Typography from '@material-ui/core/Typography';
-import JuniorFeelingsIconTableCell from 'src/containers/JuniorFeelingsIconTableCell/aaa';
+import JuniorFeelingsIconTableCell from 'src/containers/JuniorFeelingsIconTableCell/JuniorFeelingsIconTableCell';
 
 class JuniorFeelingsTableRow extends React.Component<JuniorFeelingsTableRowProps> {
   render() {
