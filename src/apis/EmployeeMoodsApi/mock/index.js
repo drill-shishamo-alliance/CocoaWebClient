68e7b77b083b4277e566c4263476b727c04b3c85
若林勇汰 <wnyuukure@gmail.com>
2019-10-05 17:36:55 +0900

    [chore] expressを追加してmockサーバを立てた

---
+++ src/apis/EmployeeMoodsApi/mock/index.js
@@ -0,0 +1,47 @@
+// expressモジュールを読み込む
+const express = require('express');
+
+// expressアプリを作成する
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+// /moods にアクセスしてきたときmood.jsonを返す
+app.get('/moods', (req, res) => {
+  res.sendFile(__dirname + '/mood.json', err => {
+    if (err) {
+      res.sendStatus(400);
+    } else {
+      console.log('sending completed');
+    }
+  });
+});
+
+app.get('/employees/moods', (req, res) => {
+  const year = req.query.year;
+  const month = req.query.month;
+
+  if (year === 2019 && month === 9) {
+    res.sendFile(__dirname + '/september_mood.json', err => {
+      if (err) {
+        res.sendStatus(400);
+      } else {
+        console.log('sending completed');
+      }
+    });
+  } else if (year === 2019 && month === 10) {
+    res.sendFile(__dirname + '/octorber_mood.json', err => {
+      if (err) {
+        res.sendStatus(400);
+      } else {
+        console.log('sending completed');
+      }
+    });
+  }
+});
+
+app.listen(8000, () => console.log('Listening on Port 8000'));
