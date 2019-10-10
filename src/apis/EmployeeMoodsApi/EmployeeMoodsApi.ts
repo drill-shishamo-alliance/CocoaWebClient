eb983e8e7891e6ff7d7378751ba8821f9a621dcf
若林勇汰 <wnyuukure@gmail.com>
2019-10-10 14:01:32 +0900

    [fix] API変更

--- src/apis/EmployeeMoodsApi/EmployeeMoodsApi.ts
+++ src/apis/EmployeeMoodsApi/EmployeeMoodsApi.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import MoodsResponse from './MoodsResponse/MoodsResponse';
 import EmployeeMoods from './EmployeeMoodsTableResponse/EmployeeMoods';
-import WeekMoods from './EmployeeMoodsTableResponse/WeekMoods';
 // import EmployeeResponse from './EmployeeMoodsTableResponse/EmployeesResponse';
 
 class EmployeeMoodsApi {
@@ -35,44 +34,41 @@ class EmployeeMoodsApi {
 
   public async getMoods(access_token?: string) {
     try {
-      const response = await axios.get<MoodsResponse>(
-        'https://virtserver.swaggerhub.com/jbsHakodate/Cocoa-Core-API/1.0.2/moods',
-        {
-          headers: {
-            access_token,
-            'Access-Control-Allow-Origin': 'http://localhost:3000',
-          },
-          responseType: 'json',
-          data: {},
-        }
-      );
+      const response = await axios.get<MoodsResponse>('http://localhost:8000/moods', {
+        // headers: {
+        //   access_token,
+        //   'Access-Control-Allow-Origin': 'http://localhost:3000',
+        // },
+        // responseType: 'json',
+        // data: {},
+      });
       return response;
     } catch (error) {
       throw new Error(error);
     }
   }
 
-  public async getEmployeeMonthMoods() {
-    try {
-      return await axios.get<WeekMoods[]>('http://localhost:3001/month', {});
-    } catch (error) {
-      throw new Error(error);
-    }
-  }
-
-  // 年と月を受け取ってqueryにのっけてリクエストを送る
-  // public async getEmployeeMonthMoods(year: number, month: number) {
+  // public async getEmployeeMonthMoods() {
   //   try {
-  //     return await axios.get<WeekMoods[]>('url', {
-  //       params: {
-  //         year: year,
-  //         month: month,
-  //       },
-  //     });
+  //     return await axios.get<WeekMoods[]>('http://localhost:3001/month', {});
   //   } catch (error) {
   //     throw new Error(error);
   //   }
   // }
+
+  // 年と月を受け取ってqueryにのっけてリクエストを送る
+  public async getEmployeeMonthMoods(year: number, month: number) {
+    try {
+      return await axios.get<EmployeeMoods[]>('http://localhost:8000/employees/moods', {
+        params: {
+          year: year,
+          month: month,
+        },
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
 }
 
 export default new EmployeeMoodsApi();
