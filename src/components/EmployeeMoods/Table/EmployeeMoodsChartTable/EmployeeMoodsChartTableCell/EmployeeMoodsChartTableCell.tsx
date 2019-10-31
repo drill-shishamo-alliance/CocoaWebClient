dcfdd64d7f6b4a0bf824e36f963c076e265c560d
ShotaroOkada <b1016117@gmail.com>
2019-10-31 19:19:25 +0900

    WIP:EmployeeMoodsChartTableCellの修正完了

--- src/components/EmployeeMoods/Table/EmployeeMoodsChartTable/EmployeeMoodsChartTableCell/EmployeeMoodsChartTableCell.tsx
+++ src/components/EmployeeMoods/Table/EmployeeMoodsChartTable/EmployeeMoodsChartTableCell/EmployeeMoodsChartTableCell.tsx
@@ -1,99 +1,52 @@
-import * as React from 'react';
-import EmployeeMoodsChartTableCellProps from './EmployeeMoodsChartTableCellProps';
+import React from 'react';
 import styles from './EmployeeMoodsChartTableCellStyles';
-import { withStyles } from '@material-ui/styles';
 import { LineChart, Line, YAxis, CartesianGrid, Tooltip, XAxis } from 'recharts';
 import EmployeeMoodsChartTableCellTickSvg from '../EmployeeMoodsChartTableCellTickSvg/EmployeeMoodsChartTableCellTickSvg';
+import { useSelector } from 'react-redux';
+import RootState from 'src/states';
 
-class EmployeeMoodsChartTableCell extends React.Component<EmployeeMoodsChartTableCellProps> {
-  render() {
-    const { classes, weekMoods, moods } = this.props;
+export type props = {
+  moodIds: string[];
+};
 
-    const attendanceMondayMoodIndex = moods.findIndex(
-      mood => mood.id === weekMoods.monday.mood_ids.attendance
-    );
+const EmployeeMoodsLineChart = (props: props) => {
+  const classes = styles();
+  const moods = useSelector((state: RootState) => state.MoodsState);
+  const data = props.moodIds.map(moodId => {
+    return { 気分: moods[moodId].name };
+  });
 
-    const attendanceMondayMoodIconWeight =
-      attendanceMondayMoodIndex >= 0 ? moods[attendanceMondayMoodIndex].weight : 0;
+  const CustomizedTicks = (props: any) => {
+    const { x, y, payload } = props;
 
-    const attendanceTuesdayMoodIndex = moods.findIndex(
-      mood => mood.id === weekMoods.tuesday.mood_ids.attendance
-    );
+    return <EmployeeMoodsChartTableCellTickSvg x={x} y={y} tick={payload.value} />;
+  };
 
-    const attendanceTuesdayMoodIconWeight =
-      attendanceTuesdayMoodIndex >= 0 ? moods[attendanceTuesdayMoodIndex].weight : 0;
+  return (
+    <div className={classes().chartPosition}>
+      <LineChart
+        width={950}
+        height={200}
+        data={data}
+        margin={{
+          top: 10,
+          right: 15,
+          left: 35,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray='3 3' />
+        <XAxis dataKey='name' tickLine={false} />
+        <YAxis
+          domain={['dataMin', 'dataMax']}
+          ticks={[0, 1, 2, 3, 4, 5]}
+          tick={<CustomizedTicks />}
+        />
+        <Tooltip />
+        <Line type='monotone' dataKey='気分' stroke='#2196f3' />
+      </LineChart>
+    </div>
+  );
+};
 
-    const attendanceWednesdayMoodIndex = moods.findIndex(
-      mood => mood.id === weekMoods.wednesday.mood_ids.attendance
-    );
-
-    const attendanceWednesdayMoodIconWeight =
-      attendanceWednesdayMoodIndex >= 0 ? moods[attendanceWednesdayMoodIndex].weight : 0;
-
-    const attendanceThursdayMoodIndex = moods.findIndex(
-      mood => mood.id === weekMoods.thursday.mood_ids.attendance
-    );
-
-    const attendanceThursdayMoodIconWeight =
-      attendanceThursdayMoodIndex >= 0 ? moods[attendanceThursdayMoodIndex].weight : 0;
-
-    const attendanceFridayMoodIndex = moods.findIndex(
-      mood => mood.id === weekMoods.friday.mood_ids.attendance
-    );
-
-    const attendanceFridayMoodIconWeight =
-      attendanceFridayMoodIndex >= 0 ? moods[attendanceFridayMoodIndex].weight : 0;
-
-    const data = [
-      {
-        気分: attendanceMondayMoodIconWeight,
-      },
-      {
-        気分: attendanceTuesdayMoodIconWeight,
-      },
-      {
-        気分: attendanceWednesdayMoodIconWeight,
-      },
-      {
-        気分: attendanceThursdayMoodIconWeight,
-      },
-      {
-        気分: attendanceFridayMoodIconWeight,
-      },
-    ];
-
-    const CustomizedTicks = (props: any) => {
-      const { x, y, payload } = props;
-
-      return <EmployeeMoodsChartTableCellTickSvg x={x} y={y} tick={payload.value} />;
-    };
-
-    return (
-      <div className={classes.chartPosition}>
-        <LineChart
-          width={950}
-          height={200}
-          data={data}
-          margin={{
-            top: 10,
-            right: 15,
-            left: 35,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray='3 3' />
-          <XAxis dataKey='name' tickLine={false} />
-          <YAxis
-            domain={['dataMin', 'dataMax']}
-            ticks={[0, 1, 2, 3, 4, 5]}
-            tick={<CustomizedTicks />}
-          />
-          <Tooltip />
-          <Line type='monotone' dataKey='気分' stroke='#2196f3' />
-        </LineChart>
-      </div>
-    );
-  }
-}
-
-export default withStyles(styles)(EmployeeMoodsChartTableCell);
+export default EmployeeMoodsLineChart;
