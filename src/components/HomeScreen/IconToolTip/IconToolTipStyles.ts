0171ff6d2c17b71b56de569bdf35150a11c8e3c0
若林勇汰 <wnyuukure@gmail.com>
2020-02-27 15:53:55 +0900

    [fix] tooltipに吹き出しを作成した

--- src/components/HomeScreen/IconToolTip/IconToolTipStyles.ts
+++ src/components/HomeScreen/IconToolTip/IconToolTipStyles.ts
@@ -3,16 +3,37 @@ import styled from 'styled-components';
 export const StyledIconToolTip = styled('div')`
   position: absolute;
   z-index: 3;
-  top: 60px;
+  top: 75px;
   left: 50%;
   transform: translateX(-50%);
   -webkit-transform: translateX(-50%);
   text-align: center;
   background: white;
-  border: solid 1px;
-  border-color: #dcdcdc;
+  border: solid 1px #dcdcdc;
   font-size: 16px;
   color: black;
+  &::after,
+  &::before {
+    bottom: 100%;
+    left: 50%;
+    font-size: 16px;
+    border: solid transparent;
+    content: ' ';
+    position: absolute;
+    pointer-events: none;
+  }
+  &::after {
+    border-color: rgba(255, 255, 255, 0);
+    border-bottom-color: #ffffff;
+    border-width: 20px;
+    margin-left: -20px;
+  }
+  &::before {
+    border-color: rgba(220, 220, 220, 0);
+    border-bottom-color: #dcdcdc;
+    border-width: 21px;
+    margin-left: -21px;
+  }
 `;
 
 export const Material = styled('i')`
