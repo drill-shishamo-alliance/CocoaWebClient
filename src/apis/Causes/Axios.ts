2f7bfbf963d5764f933716a0404c75547dcbcc9d
若林勇汰 <wnyuukure@gmail.com>
2020-03-21 14:49:44 +0900

    [fix] 原因取得を本番環境に移行

---
+++ src/apis/Causes/Axios.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export default axios.create({
+  headers: {
+    post: {
+      'Access-Control-Allow-Origin': 'http://localhost:3000',
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+  },
+  baseURL: process.env.REACT_APP_COCOA_CAUSE_API,
+});
--- src/apis/Causes/GetCausesOnAjax.ts
+++ src/apis/Causes/GetCausesOnAjax.ts
@@ -1,19 +1,15 @@
-import Axios from '../Axios';
+import Axios from './Axios';
 import Cause from './Model';
 
 export type getCausesParams = {
-  department_id?: number;
+  departmentId: number;
 };
 
 export type getCausesResponse = Cause[];
 
-export async function getCausesOnAjax({ department_id }: getCausesParams) {
+export async function getCausesOnAjax({ departmentId }: getCausesParams) {
   try {
-    return await Axios.get<getCausesResponse>('/causes', {
-      params: {
-        department_id,
-      },
-    });
+    return await Axios.get<getCausesResponse>(`/causes/${departmentId}`);
   } catch (e) {
     throw new Error(e);
   }
